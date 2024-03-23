267b24eaa49e8cd963e0247d28f73700a68bdabd
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-03-23 17:29:08 +0900

    ソース整理。

--- src/app/defer-and-from-sample/defer-and-from-sample.component.ts
+++ src/app/defer-and-from-sample/defer-and-from-sample.component.ts
@@ -44,20 +44,22 @@ export class DeferAndFromSampleComponent {
     this.deferEndTimeWithConst = '';
 
     // defer を使用して pipe 内で Promise を呼び出す。
-    of('').pipe(
-      delay(3000),
-      mergeMap(() => defer(() => this.getDateByPromise()))
-    )
+    of('')
+      .pipe(
+        delay(3000),
+        mergeMap(() => defer(() => this.getDateByPromise()))
+      )
       .subscribe(value => {
         this.deferEndTime = value.toLocaleString();
       });
 
     // defer を使用して外部定義した Promise を呼び出す。
     const process: Observable<Date> = defer(() => this.getDateByPromise());
-    of('').pipe(
-      delay(3000),
-      mergeMap(() => process)
-    )
+    of('')
+      .pipe(
+        delay(3000),
+        mergeMap(() => process)
+      )
       .subscribe(value => {
         this.deferEndTimeWithConst = value.toLocaleString();
       });
@@ -72,10 +74,11 @@ export class DeferAndFromSampleComponent {
     this.fromEndTimeWithConst = '';
 
     // from を使用して pipe 内で Promise を呼び出す。
-    of('').pipe(
-      delay(3000),
-      mergeMap(() => from(this.getDateByPromise()))
-    )
+    of('')
+      .pipe(
+        delay(3000),
+        mergeMap(() => from(this.getDateByPromise()))
+      )
       .subscribe(value => {
         this.fromEndTime = value.toLocaleString();
       });
@@ -83,10 +86,11 @@ export class DeferAndFromSampleComponent {
     // from を使用して外部定義した Promise を呼び出す。
     // subscribe は delay で指定したタイミングだが、表示される終了日時が from が定義された日時になっている。
     const process: Observable<Date> = from(this.getDateByPromise());
-    of('').pipe(
-      delay(3000),
-      mergeMap(() => process)
-    )
+    of('')
+      .pipe(
+        delay(3000),
+        mergeMap(() => process)
+      )
       .subscribe(value => {
         this.fromEndTimeWithConst = value.toLocaleString();
       });
